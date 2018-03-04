34307ed0563e01ebba890fc0e1cc3de1085b4acb
ahmedtarek2134 <ahmedtarek3214@gmail.com>
2018-03-04 21:31:23 +0300

    Done everything for now :pray:

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Wrapper from '../components/shared/PageWrapper'
+import Header from '../components/shared/PageHeader'
 
 const About = () => (
-  <div>
-    <h1>About Page</h1>
-  </div>
+  <Wrapper>
+    <Header>About Page</Header>
+    <Link to='/'>Go to home page</Link>
+  </Wrapper>
 )
 
 export default About
