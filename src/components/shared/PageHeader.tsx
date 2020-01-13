a9c49fa9f9261979ec0150243d5fb1974e1403e0
dtp263 <olin.davis20@gmail.com>
2020-01-12 22:45:04 -0500

    add material ui styles, user app bar for top nacv

--- src/components/shared/PageHeader.tsx
+++ src/components/shared/PageHeader.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
-import { Menu, MenuItem, Button, Grid } from '@material-ui/core'
-
+import { Menu, MenuItem, Button, Grid, AppBar } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 const PageHeader = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const classes = useStyles({})
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
@@ -16,32 +29,34 @@ const PageHeader = () => {
   };
 
   return (
-    <header>
-      <Grid container
-        direction="row"
-        justify="flex-end"
-        alignItems="center">
-        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-          <MenuIcon></MenuIcon>
-        </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose} component={Link} to="/">
-            Home
+    <header className={classes.root}>
+      <AppBar position="static">
+        <Grid container
+          direction="row"
+          justify="flex-end"
+          alignItems="center">
+          <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <MenuIcon></MenuIcon>
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose} component={Link} to="/">
+              Home
         </MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/about">
-            About
+            <MenuItem onClick={handleClose} component={Link} to="/about">
+              About
         </MenuItem>
-          <MenuItem onClick={handleClose} component={Link} to="/device-management">
-            Device Management
+            <MenuItem onClick={handleClose} component={Link} to="/device-management">
+              Device Management
         </MenuItem>
-        </Menu>
-      </Grid>
+          </Menu>
+        </Grid>
+      </AppBar>
 
     </header>
   )
--- src/pages/DeviceManagementPage.tsx
+++ src/pages/DeviceManagementPage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Subscribe } from 'unstated-typescript'
 import { Link } from 'react-router-dom'
-import { List, ListItem } from '@material-ui/core'
+import { List, ListItem, Container } from '@material-ui/core'
 import DeviceManagement from '../containers/DeviceManagementContainer'
 import Wrapper from '../components/shared/PageWrapper'
 import PageHeader from '../components/shared/PageHeader'
@@ -9,18 +9,20 @@ import PageHeader from '../components/shared/PageHeader'
 const DeviceManagementPage = () => (
   <Wrapper>
     <PageHeader />
-    <List>
-      <Subscribe to={[DeviceManagement]}>
+    <Container maxWidth="md">
+      <List>
+        <Subscribe to={[DeviceManagement]}>
 
-        {deviceManager =>
-          deviceManager.state.devices.map((device) => (
-            <ListItem button key={device.NodeId}>
-              {device.NodeIPAddr}:{device.Port}
-            </ListItem>
-          ))
-        }
-      </Subscribe>
-    </List>
+          {deviceManager =>
+            deviceManager.state.devices.map((device) => (
+              <ListItem button key={device.NodeId}>
+                {device.NodeIPAddr}:{device.Port}
+              </ListItem>
+            ))
+          }
+        </Subscribe>
+      </List>
+    </Container>
   </Wrapper >
 )
 
